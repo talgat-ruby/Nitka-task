a1940dc51a9c21f3ea75b28dafcb3b7643e176b4
Talgat S <talgat.s@protonmail.com>
2019-07-27 16:51:13 +0600

    Move App component, add router and Home

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Switch, Route} from 'react-router';
+
+import Home from '../Home';
+
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/" component={Home} />
+				{/*<Route path="/:id" component={About} />
+								<Route component={NoMatch} />*/}
+			</Switch>
+		</div>
+	);
+}
+
+export default App;
