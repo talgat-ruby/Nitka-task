dd259ae5cb8b628db1c7391f2b70433b23cfc341
Talgat S <talgat.s@protonmail.com>
2019-07-29 04:26:15 +0600

    Updates

---
+++ precache-manifest.b670cafa133193fec7ab6eebc09a4d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84dba83704e16c636ee4617aa0515c15",
+    "url": "/nitka-task/index.html"
+  },
+  {
+    "revision": "cc8c3c9b5170790ead63",
+    "url": "/nitka-task/static/css/2.cb759328.chunk.css"
+  },
+  {
+    "revision": "9ca13cd870ab94a6303d",
+    "url": "/nitka-task/static/css/main.74496e4f.chunk.css"
+  },
+  {
+    "revision": "cc8c3c9b5170790ead63",
+    "url": "/nitka-task/static/js/2.718fddba.chunk.js"
+  },
+  {
+    "revision": "9ca13cd870ab94a6303d",
+    "url": "/nitka-task/static/js/main.2f605c6f.chunk.js"
+  },
+  {
+    "revision": "66981bcc080ab10a9491",
+    "url": "/nitka-task/static/js/runtime~main.28d3fae9.js"
+  }
+]);
