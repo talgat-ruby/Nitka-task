5a26ea5f33939d3fb774ae46bf135a95c03463a8
Talgat S <talgat.s@protonmail.com>
2019-07-29 02:31:41 +0600

    Ass Hotel page

--- src/components/Hotel/Hotel.js
+++ src/components/Hotel/Hotel.js
@@ -1,25 +1,41 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
-import {Layout, Breadcrumb} from 'antd';
+import {Link} from 'react-router-dom';
+import {Layout, Breadcrumb, Card} from 'antd';
+
+import {getHotel} from '../../redux/selectors/';
 
 import './Hotel.css';
 
-function Hotel() {
+function Hotel({hotel = {}}) {
 	return (
 		<Layout>
-			<Layout.Header>
-				<Breadcrumb></Breadcrumb>
+			<Layout.Header className="Hotel-Header">
+				<Breadcrumb className="Hotel-Breadcrumb">
+					<Breadcrumb.Item key="home">
+						<Link to="/" className="Hotel-Home">
+							Home
+						</Link>
+					</Breadcrumb.Item>
+				</Breadcrumb>
 			</Layout.Header>
-			<Layout.Content className="Home-Content"></Layout.Content>
+			<Layout.Content>
+				<Card
+					style={{width: 240}}
+					cover={<img alt={hotel.name} src={hotel.photo} />}
+				>
+					<Card.Meta title={hotel.name} description={hotel.description} />
+				</Card>
+			</Layout.Content>
 		</Layout>
 	);
 }
 
-const mapStateToProps = state => ({});
-
-const mapDispatchToProps = {};
+const mapStateToProps = (state, ownProps) => ({
+	hotel: getHotel(state, ownProps)
+});
 
 export default connect(
 	mapStateToProps,
-	mapDispatchToProps
+	null
 )(Hotel);
--- src/redux/selectors/hotels.js
+++ src/redux/selectors/hotels.js
@@ -6,3 +6,11 @@ export const hotelsList = createSelector(
 	hotels,
 	hotels => hotels.list
 );
+
+const getIdFromLocation = (_, props) => props.match.params.id;
+
+export const getHotel = createSelector(
+	hotelsList,
+	getIdFromLocation,
+	(list, hotelId) => list.find(({id}) => id === Number(hotelId))
+);
